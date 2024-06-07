0848bd0b66445fc63c6b0c167141235482500da4
hemaet <hemaet33@gmail.com>
2024-06-07 20:25:35 +0600

    created about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4 p-3'>
+        <h1 className='text-2xl text-amber-600 font-medium'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas itaque, voluptatum iusto vel voluptas? A repellat aut, voluptatum natus similique nostrum culpa explicabo facilis enim ullam ipsum commodi amet. Maxime, atque odit saepe tenetur explicabo possimus cumque, dolorum officia accusamus iusto eum velit animi, vel voluptas obcaecati asperiores a!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia obcaecati dicta nostrum deserunt hic culpa aliquid veritatis fuga tempora odit quam minus quasi, consectetur blanditiis eum repellat laudantium dignissimos rerum, ex veniam numquam ea autem iure? Cumque expedita, numquam ad consequatur ipsam facere obcaecati assumenda soluta error nobis mollitia eos nihil eum pariatur deserunt earum a natus minus! Consequuntur, qui!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil est possimus quis atque ducimus ipsam inventore! A excepturi dicta vel autem rem sit dolor ut, amet illo consequatur modi ad sunt qui accusantium iure odit ipsam quae vero officiis ipsa?</p>
+    </div>
+  )
+}
+
+export default About
